651173d9de605b7929cbacc418ba84d7262b7dab
jezman <dev.jezman@gmail.com>
2018-09-26 10:03:33 +0300

    Init commit.

---
+++ spec/smtp2go_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe Smtp2go do
+  it "has a version number" do
+    expect(Smtp2go::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
